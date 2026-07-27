2c0e265f9e6608e4bfdb530a0ae92ad4987e1de3
huangnanguang <huangng@didihu.com.cn>
2026-07-27 21:45:39 +0800

    Add shareable runtime config

---
+++ config.js
@@ -0,0 +1,16 @@
+window.MEETING_HOST_CONFIG = {
+  coze: {
+    baseUrl: "https://api.coze.cn",
+    pat: "pat_xxx",
+    botId: "",
+    workflows: {
+      generateMinutes: "workflow_id_for_wf_generate_minutes",
+      saveMinutes: "workflow_id_for_wf_save_minutes",
+      searchMinutes: "workflow_id_for_wf_search_minutes",
+      receiveTranscript: ""
+    }
+  },
+  app: {
+    meetingTitle: "产品最小功能验证会议"
+  }
+};
